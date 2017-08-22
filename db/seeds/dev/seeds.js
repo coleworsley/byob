d7dd27128ae23097417c2ed12c2a7a4e3b361e0b
Ciara Bujanos <c.buj22@gmail.com>
2017-08-22 14:36:07 -0600

    breweries database inserted

--- db/seeds/dev/seeds.js
+++ db/seeds/dev/seeds.js
@@ -3,7 +3,7 @@ const breweries = require('../../Data/breweries.json');
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  knex('breweries').del()
+  return knex('breweries').del()
     .then(() => {
       return Promise.all(breweries.map((brewery) => {
         return knex('breweries').insert(brewery);
