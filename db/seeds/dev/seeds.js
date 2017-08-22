40ca8321fb626e138177cc1349aad7aa2308a151
Ciara Bujanos <c.buj22@gmail.com>
2017-08-22 14:31:47 -0600

    start seeding

---
+++ db/seeds/dev/seeds.js
@@ -0,0 +1,12 @@
+const brews = require('../../Data/beers.json');
+const breweries = require('../../Data/breweries.json');
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  knex('breweries').del()
+    .then(() => {
+      return Promise.all(breweries.map((brewery) => {
+        return knex('breweries').insert(brewery);
+      }))
+    })
+};
