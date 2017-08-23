8e7a8a2acdbff927ebc12d4a4ebf0e39309a8795
Ciara Bujanos <c.buj22@gmail.com>
2017-08-23 13:50:41 -0600

    write first passing test

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('./knexfile')[env];
 const db = require('knex')(config);
 
+
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,7 +1,7 @@
 /* eslint-env mocha */
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../server');
+const { app: server, db } = require('../server');
 
 // eslint-disable-next-line
 const should = chai.should();
@@ -9,7 +9,7 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
-  it.skip('should return the homepage', (done) => {
+  it('should return the homepage', (done) => {
     chai.request(server)
       .get('/')
       .end((err, res) => {
