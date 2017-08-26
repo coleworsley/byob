410a31d8268fb0055840704abe5545331e6ce818
coleworsley <cole.s.worsley@gmail.com>
2017-08-26 13:24:20 -0600

    add test for valid token

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 const port = (process.env.PORT || 3000);
 const env = process.env.NODE_ENV || 'development';
-const config = require('./knexfile')[env];
+const config = require('../knexfile')[env];
 const db = require('knex')(config);
 
 const checkAuth = ((req, res, next) => {
