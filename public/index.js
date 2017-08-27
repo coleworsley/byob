7a1d3b616a29dd4d370d32d7c175cac0bc4e4b50
Ciara Bujanos <c.buj22@gmail.com>
2017-08-27 16:42:46 -0600

    front end done

--- public/index.js
+++ public/index.js
@@ -1,8 +1,22 @@
-const submit = document.querySelector('button');
 const email = document.querySelector('.email');
 const app = document.querySelector('.app');
 
-submit.addEventListener('click', (e) => {
+$('button').on('click', (e) => {
   e.preventDefault();
-  console.log('here');
+  $('.token-container').addClass('white');
+  fetch('/auth', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      email: email.value,
+      appName: app.value,
+    }),
+  })
+    .then(res => res.json())
+    .then((token) => {
+      $('.token-container').append(`<div class="token">Your token:${token.token}</div>`);
+    })
+    .catch(error => console.log(error));
 });
