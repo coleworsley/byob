bf33678802c4283568f067f12488b25b78f86a36
Ciara Bujanos <c.buj22@gmail.com>
2017-08-27 15:45:30 -0600

    frontend

---
+++ public/index.js
@@ -0,0 +1,8 @@
+const submit = document.querySelector('button');
+const email = document.querySelector('.email');
+const app = document.querySelector('.app');
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('here');
+});
